6302c42e35b6b3e1b8df4053e099de6e3f0364b2
Carlator <clorenzozuniga@gmail.com>
2023-10-21 15:47:29 +0200

    - Added announcement, suggestion and poll models

--- backend/feed/admin.py
+++ backend/feed/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Announcement)
+admin.site.register(models.Suggestion)
+admin.site.register(models.Poll)
+admin.site.register(models.PollOption)
--- backend/feed/models.py
+++ backend/feed/models.py
@@ -2,5 +2,42 @@ from django.db import models
 
 # Create your models here.
 class Announcement(models.Model):
-    message = models.CharField(max_length=2500)
-    created = models.DateField(auto_now=True)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    date_created = models.DateField(auto_now=True)
+    owner = models.ForeignKey('users.User', related_name='announcements', on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    
+class Suggestion(models.Model):
+    body = models.CharField(max_length=250)
+    likes = models.PositiveSmallIntegerField(default=0)
+    date_created = models.DateField(auto_now=True)
+    owner = models.ForeignKey('users.User', related_name='suggestions', on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"{self.body} - {self.likes}"
+    
+    
+
+class Poll(models.Model):
+    question = models.CharField(max_length=100)
+    date_created = models.DateField(auto_now=True)
+    owner = models.ForeignKey('users.User', related_name='poll', on_delete=models.CASCADE)
+    
+    
+    def __str__(self) -> str:
+        return self.question
+
+class PollOption(models.Model):
+    body = models.CharField(max_length=100)
+    date_created = models.DateField(auto_now=True)
+    likes = models.PositiveSmallIntegerField(default=0)
+    poll = models.ForeignKey('Poll', related_name='polloption', on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.body
+    
+    
