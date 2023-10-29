9717e8d5a46476d90c6a70290b1ed0f2444a5da3
Carlator <clorenzozuniga@gmail.com>
2023-10-29 17:23:42 +0100

    - beautyfied login form

--- frontend/src/pages/components/Login.jsx
+++ frontend/src/pages/components/Login.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faLock, faEnvelope, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Login({user, setUser, client}) {
 
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
 	function handleLogIn(e) {
         e.preventDefault();
@@ -46,26 +51,36 @@ export default function Login({user, setUser, client}) {
     return (
         <>
 			<form onSubmit={handleLogIn} id='login-form'>
-				<h2>Login</h2> 
-				<input 
-				type="text" 
-				id="email"
-				placeholder="email"
-				autoComplete="off"
-				value={email}
-				onChange={(e) => {setEmail(e.target.value)}}/>
+                
+                <div className='shadow login-field-container'>
+                    <label htmlFor="email"><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></label>
+                    <input 
+                    type="text" 
+                    id="email"
+                    className='login-field'
+                    placeholder="email"
+                    autoComplete="off"
+                    value={email}
+                    onChange={(e) => {setEmail(e.target.value)}}/>
+                </div>
+                
+                <div className='shadow login-field-container'>
+                    <label htmlFor="password"><FontAwesomeIcon icon={faLock}></FontAwesomeIcon></label>
+                    <input 
+                    type={showPassword ? "text" : "password"} 
+                    id="password"
+                    className='login-field'
+                    placeholder="password"
+                    autoComplete="none"
+                    value={password}
+                    onChange={(e) => {setPassword(e.target.value)}}/>
+                    <FontAwesomeIcon onClick={(e) => setShowPassword(!showPassword)} icon={showPassword ? faEyeSlash : faEye}></FontAwesomeIcon>
+
+                </div>
+               
 
 
-				<input 
-				type="password" 
-				id="password"
-				placeholder="password"
-				autoComplete="none"
-				value={password}
-				onChange={(e) => {setPassword(e.target.value)}}/>
-
-
-				<button className='border'><h4>Submit</h4></button>
+				<button type="submit" className='submit shadow'><h2>Login</h2></button>
 				
 			</form>
 		
