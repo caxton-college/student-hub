3fab28d6aa317d8cdd18811f6728f4f079cfdda6
Carlos <clorenzozuniga@gmail.com>
2024-03-25 08:55:45 +0100

    - Updated createuser script

--- backend/create_users.py
+++ backend/create_users.py
@@ -15,112 +15,118 @@ from tqdm import tqdm
 from users.models import User
 
 def email_creds(users: List[Dict[str, str]]) -> None:
-    
-    env = environ.Env()
-    environ.Env.read_env()
-    
-    smtp_server = 'smtp.gmail.com'
-    smtp_port = 587
-    smtp_username = env("email")
-    smtp_password = env("passkey")
-    
-    
+	
+	env = environ.Env()
+	environ.Env.read_env()
+	
+	smtp_server = 'smtp.gmail.com'
+	smtp_port = 587
+	smtp_username = env("email")
+	smtp_password = env("passkey")
+	
+	
 
-    with smtplib.SMTP(smtp_server, smtp_port) as smtp:
-        smtp.starttls()
-        smtp.login(smtp_username, smtp_password)
-        
-        for user in tqdm(users):
-            recipient = user.get("email", "")
-            password = user.get("password", "")
-            name = user.get("name", "")
-            
-            from_email = smtp_username
-            
-            subject = "Welcome to Student Hub!"
-            
-            body = f"""
-            <html>
-              <body>
-                <p>Hi {name},</p>
-                <p>We are thrilled to welcome you to Student Hub! 🎉</p>
-                <p>Your login credentials are as follows:</p>
-                <ul>
-                  <li><strong>Username:</strong> {recipient}</li>
-                  <li><strong>Password:</strong> {password}</li>
-                </ul>
-                <p>You can access the Student Hub platform using the following link: <a href="http://www.studenthub.caxtoncollege.local">Student Hub Platform</a></p>
-                <p>We hope it imporves school and allows you to win prizes ;)</p>
-                <p>Best regards,<br/>Carlos Lorenzo</p>
-              </body>
-            </html>
-            """         
+	with smtplib.SMTP(smtp_server, smtp_port) as smtp:
+		smtp.starttls()
+		smtp.login(smtp_username, smtp_password)
+		
+		for user in tqdm(users):
+			recipient = user.get("email", "")
+			password = user.get("password", "")
+			name = user.get("name", "")
+			
+			from_email = smtp_username
+			
+			subject = "Welcome to Student Hub!"
+			
+			body = f"""
+			<html>
+			  <body>
+				<p>Hi {name},</p>
+				<p>We are thrilled to welcome you to Student Hub! 🎉</p>
+				<p>Your login credentials are as follows:</p>
+				<ul>
+				  <li><strong>Username:</strong> {recipient}</li>
+				  <li><strong>Password:</strong> {password}</li>
+				</ul>
+				<p>You can access the Student Hub platform using the following link: http://studenthub.caxtoncollege.local/</p>
+				<p>We hope it imporves school and allows you to win prizes. Note that the app will only be accessible when connected to the school Wi-Fi.</p>
+				<p>Best regards,<br/>Carlos Lorenzo on behalf of Caxton College</p>
+			  </body>
+			</html>
+			"""         
 
-            msg = MIMEMultipart()
-            msg["From"] = from_email
-            msg["To"] = recipient
-            msg["Subject"] = subject
-            msg.attach(MIMEText(body, 'html'))
-            
-            smtp.send_message(msg)
+			msg = MIMEMultipart()
+			msg["From"] = from_email
+			msg["To"] = recipient
+			msg["Subject"] = subject
+			msg.attach(MIMEText(body, 'html'))
+			
+			smtp.send_message(msg)
 
-        
+		
 def generate_password(length: int) -> str:
-    
-    return "".join([choice(string.ascii_letters) for _ in range(length)])
-        
+	
+	return "".join([choice(string.ascii_letters) for _ in range(length)])
+		
 def create_user(email: str,
-                name: str,
-                surname: str,
-                role: int,
-                year: int,
-                form: int,
-                house: int) -> Dict[str, str]:
-    
-    password = generate_password(8)
-    
-    
-    new_user = User.objects.create(
-        email=email,
-        name=name,
-        surname=surname,
-        password=make_password(password),
-        role=role,
-        year=year,
-        form=form,
-        house=house
-    )
-    
-    new_user.save()
-    
-    
-    
-    
-    return {
-        "name": name,
-        "email": email,
-        "password": password
-    }
-    
+				name: str,
+				surname: str,
+				role: int,
+				year: int,
+				form: int,
+				house: int) -> Dict[str, str]:
+	
+	password = generate_password(8)
+	
+	
+	new_user = User.objects.create(
+		email=email,
+		name=name,
+		surname=surname,
+		password=make_password(password),
+		role=role,
+		year=year,
+		form=form,
+		house=house
+	)
+	
+	new_user.save()
+	
+	
+	
+	
+	return {
+		"name": name,
+		"email": email,
+		"password": password
+	}
+	
 
 def populate_db() -> None:
-    df = pd.read_csv("test.csv")
-    users = []
-    
-    print("Creating users...")
-    for i, row in tqdm(df.iterrows()):
-        try:
-          users.append(create_user(email=row["email"],
-                                  name=row["name"],
-                                  surname=row["surname"],
-                                  role=row["role"],
-                                  form=row["form"],
-                                  year=row["year"],
-                                  house=row["house"]))
-        except Exception as e:
-          print(e)
-          print(f"Failed to create user: {i}, {row}")
-          continue
-        
-    print("Emailing credentials...")
-    email_creds(users=users)
+	df = pd.read_csv("studentdata.csv")
+	users = []
+	
+	print("Creating users...")
+	for i, row in tqdm(df.iterrows()):
+		try:
+			users.append(create_user(email=row["email"],
+									name=row["name"],
+									surname=row["surname"],
+									role=row["role"],
+									form=row["form"],
+									year=row["year"],
+									house=row["house"]))
+			
+		except Exception as e:
+			print(e)
+			print(f"{i}. Failed to create user {row['email']}")
+			continue
+		
+		if i == 3:
+			break
+		
+		
+		
+	print("Emailing credentials...")
+	email_creds(users=users)
